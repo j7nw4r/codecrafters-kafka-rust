e4e39c0e9cd221c686d85f69098984936f839973
Johnathan W <j7n.w4r@gmail.com>
2024-12-03 09:24:06 -0500

    Code reorganization.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+mod message;
 
-    // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+use std::io;
+use std::io::Read;
+use tokio::net::{TcpListener, TcpStream};
+use anyhow::{bail, Context};
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+const ADDRESS: &str = "127.0.0.1:9092";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind(ADDRESS)
+        .await
+        .context("could not bind to address")?;
+
+    while let Ok((stream, _)) = listener.accept().await {
+        handle_stream_result(stream)?;
     }
+    Ok(())
+}
+
+fn handle_stream_result (_tcp_stream: TcpStream) -> anyhow::Result<()> {
+    Ok(())
 }
