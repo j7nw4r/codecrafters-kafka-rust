7b7397cc55f5ce2e5c87e5a1ccb32803c2f73fbe
Johnathan W <j7n.w4r@gmail.com>
2024-12-03 09:36:35 -0500

    Returning 7.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::io;
 use std::io::Read;
 use tokio::net::{TcpListener, TcpStream};
 use anyhow::{bail, Context};
+use bytes::{BufMut, BytesMut};
+use tokio::io::AsyncWriteExt;
 
 const ADDRESS: &str = "127.0.0.1:9092";
 
@@ -16,11 +18,17 @@ async fn main() -> anyhow::Result<()> {
         .context("could not bind to address")?;
 
     while let Ok((stream, _)) = listener.accept().await {
-        handle_stream_result(stream)?;
+        handle_stream_result(stream).await?;
     }
     Ok(())
 }
 
-fn handle_stream_result (_tcp_stream: TcpStream) -> anyhow::Result<()> {
+async fn handle_stream_result (mut tcp_stream: TcpStream) -> anyhow::Result<()> {
+    let (_read_half, mut write_half) = tcp_stream.split();
+
+    let mut response_bytes = BytesMut::new();
+    response_bytes.put_i32(7);
+    let _ = write_half.write(response_bytes.iter().as_slice()).await
+        .context("could not write to response")?;
     Ok(())
 }
